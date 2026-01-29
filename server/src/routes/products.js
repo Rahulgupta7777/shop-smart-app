ed113dfc7551e2a3ff740ef42801a986239f7b99
Rahul Gupta <rahulgupta193246@gmail.com>
2026-01-29 12:00:00 +0530

    feat(api): Refactor products routing logic and input validation

--- server/src/routes/products.js
+++ server/src/routes/products.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const { PrismaClient } = require('@prisma/client');
+const prisma = require('../lib/prisma');
 
 const router = express.Router();
-const prisma = new PrismaClient();
 
 // GET all products
 router.get('/', async (req, res, next) => {
