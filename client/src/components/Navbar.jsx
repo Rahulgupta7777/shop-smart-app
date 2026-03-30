9dd8cb813089166c921c16155a0054104c47c227
Rahul Gupta <rahulgupta193246@gmail.com>
2026-03-30 18:46:40 +0530

    Update client/src/components/Navbar.jsx

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <h1>ShopSmart</h1>
+      </div>
+      <div className="navbar-links">
+        <span className="navbar-tagline">Smart Shopping, Simple Life</span>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
